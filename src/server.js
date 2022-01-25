baaf6cb136750baefae2aeb1fca3c471e57c59a6
yonatanski <phpcodeyg@gmail.com>
2022-01-25 15:58:20 +0100

    every end point works well😎

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express" // 3RD PARTY MODULE DOES NEED TO INSTALL
+import listEndpoints from "express-list-endpoints"
+import authorsRouter from "./services/authors/index.js"
+
+const server = express() // declearing server
+server.use(express.json()) // if this not worte here the request body will be undifend
+
+// ************************** ENDPOINT ****************
+server.use("/authors", authorsRouter)
+
+console.table(listEndpoints(server)) // TO PRINT THE END POINT TABLE ON TERMINAL
+
+const port = 3001
+server.listen(port, () => {
+  console.log("SEREVER IS RUNNING ON PORT", port)
+})
